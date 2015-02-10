79b1eb60eebfe6a0b4c3c04efe64c3d06ca3d915
ishikawa-tatsuya <ishikawa-tatsuya@codeer.co.jp>
2015-02-11 01:39:31 +0900

    作成

---
+++ Project/Target/MainForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Target
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        //新しいドメインでフォーム起動
+        static int i = 0;
+        public void StartNewDomain()
+        {
+            AppDomain.CreateDomain("new domain " + i++).DoCallBack(()=>new Form().Show());
+        }
+    }
+}
---
+++ Project/Test/AppDomainTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Codeer.Friendly.Dynamic;
+using Codeer.Friendly.Windows;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace Test
+{
+    [TestClass]
+    public class AppDomainTest
+    {
+        WindowsAppFriend _app;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _app = new WindowsAppFriend(Process.Start("Target.exe"));
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            Process.GetProcessById(_app.ProcessId).Kill();
+        }
+
+        [TestMethod]
+        public void Test()
+        {
+            dynamic form = _app.Type<Application>().OpenForms[0];
+
+            //新しいドメイン開始
+            form.StartNewDomain();
+
+            //別のドメインを取得
+            var apps = _app.AttachOtherDomains();
+
+            Assert.AreEqual(1, apps.Length);
+            Assert.AreEqual("new domain 0", (string)apps[0].Type<AppDomain>().CurrentDomain.FriendlyName);
+        }
+    }
+}
